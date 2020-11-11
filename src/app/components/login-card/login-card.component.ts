fca23119ed5f85bac6ac08581e92b6955ae47cd2
shivanshk <shivansh.kuchchal@dltlabs.io>
2020-11-11 13:55:03 +0530

    Form validations and actions added.

--- src/app/components/login-card/login-card.component.ts
+++ src/app/components/login-card/login-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-card',
@@ -6,7 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-card.component.css'],
 })
 export class LoginCardComponent implements OnInit {
-  constructor() {}
+  public loginForm: FormGroup;
+  public isLoading = false;
+
+  constructor(private formBuiler: FormBuilder) {
+    this.loginForm = this.formBuiler.group({
+      password: ['', Validators.required],
+    });
+  }
 
   ngOnInit() {}
+
+  async login(): Promise<void> {
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    this.setLoading(true);
+
+    try {
+    } catch (err) {}
+
+    this.setLoading(false);
+  }
+
+  private setLoading(state: boolean): void {
+    this.isLoading = state;
+    state ? this.loginForm.disable() : this.loginForm.enable();
+  }
 }
